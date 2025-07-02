c22516b8618c4cd8af884dedaaf208aed860dbb9
Fabiana Cecin <fcecin@gmail.com>
2025-07-02 13:41:53 -0300

    move hex encode/decode to the outside of logkv::Bytes

--- include/logkv/bytes.h
+++ include/logkv/bytes.h
@@ -26,9 +26,58 @@ constexpr std::array<int, 256> createHexLookup() {
 }
 static const char hexEncodeLookupUpper[] = "0123456789ABCDEF";
 static const char hexEncodeLookupLower[] = "0123456789abcdef";
+static constexpr std::array<int, 256> hexLookup = createHexLookup();
 } // namespace logkv_detail
-#include <cassert>
+
 namespace logkv {
+
+void encodeHex(char* dest, size_t dest_len, const char* src, size_t src_len,
+               bool upper = false) {
+  if (!src_len) {
+    return;
+  }
+  if (!dest || !src) {
+    throw std::invalid_argument("Null buffer and nonzero source length.");
+  }
+  const size_t len = src_len * 2;
+  if (dest_len < len) {
+    throw std::invalid_argument("Destination buffer too small.");
+  }
+  const char* lookup = upper ? logkv_detail::hexEncodeLookupUpper
+                             : logkv_detail::hexEncodeLookupLower;
+  for (size_t i = 0; i < src_len; ++i) {
+    const uint8_t byte = static_cast<uint8_t>(src[i]);
+    dest[i * 2] = lookup[byte >> 4];
+    dest[i * 2 + 1] = lookup[byte & 0x0F];
+  }
+}
+
+void decodeHex(char* dest, size_t dest_len, const char* src, size_t src_len) {
+  if (!src_len) {
+    return;
+  }
+  if (!dest || !src) {
+    throw std::invalid_argument("Null buffer and nonzero source length.");
+  }
+  if (src_len % 2 != 0) {
+    throw std::invalid_argument(
+      "Hex string must have an even number of characters.");
+  }
+  const size_t len = src_len / 2;
+  if (dest_len < len) {
+    throw std::invalid_argument("Destination buffer too small.");
+  }
+  for (size_t i = 0; i < len; ++i) {
+    const unsigned char hchar = src[2 * i];
+    const unsigned char lchar = src[2 * i + 1];
+    const int hval = logkv_detail::hexLookup[hchar];
+    const int lval = logkv_detail::hexLookup[lchar];
+    if (hval == -1 || lval == -1) {
+      throw std::invalid_argument("Hex string has invalid characters");
+    }
+    dest[i] = static_cast<char>((hval << 4) | lval);
+  }
+}
 
 /**
  * A dynamic byte array utility class that can be used as K, V in
@@ -38,9 +87,6 @@ class Bytes : public Object {
 private:
   char* data_;
   size_t size_;
-
-  static constexpr std::array<int, 256> hex_lookup =
-    logkv_detail::createHexLookup();
 
 public:
   virtual bool empty() const { return !size_; }
@@ -79,25 +125,8 @@ public:
   }
 
   static Bytes decodeHex(const char* hexData, size_t hexSize) {
-    if (hexSize == 0) {
-      return Bytes();
-    }
-    if (hexSize % 2 != 0) {
-      throw std::invalid_argument(
-        "Hex string must have an even number of characters.");
-    }
-    size_t bytesSize = hexSize / 2;
-    Bytes result(bytesSize);
-    for (size_t i = 0; i < bytesSize; ++i) {
-      const unsigned char hchar = hexData[2 * i];
-      const unsigned char lchar = hexData[2 * i + 1];
-      const int hval = hex_lookup[hchar];
-      const int lval = hex_lookup[lchar];
-      if (hval == -1 || lval == -1) {
-        throw std::invalid_argument("Hex string has invalid characters");
-      }
-      result.data_[i] = static_cast<char>((hval << 4) | lval);
-    }
+    Bytes result(hexSize / 2);
+    logkv::decodeHex(result.data(), result.size(), hexData, hexSize);
     return result;
   }
 
@@ -106,30 +135,12 @@ public:
   }
 
   static Bytes decodeHex(const Bytes& hexBytes) {
-    return decodeHex(hexBytes.data_, hexBytes.size_);
+    return decodeHex(hexBytes.data(), hexBytes.size());
   }
 
   static Bytes encodeHex(const char* data, size_t size, bool upper = false) {
-    if (!data || !size) {
-      return Bytes();
-    }
-    size_t bytesSize = size * 2;
-    Bytes result(bytesSize);
-    if (upper) {
-      for (size_t i = 0; i < size; ++i) {
-        const uint8_t byte = static_cast<uint8_t>(data[i]);
-        result.data_[i * 2] = logkv_detail::hexEncodeLookupUpper[byte >> 4];
-        result.data_[i * 2 + 1] =
-          logkv_detail::hexEncodeLookupUpper[byte & 0x0F];
-      }
-    } else {
-      for (size_t i = 0; i < size; ++i) {
-        const uint8_t byte = static_cast<uint8_t>(data[i]);
-        result.data_[i * 2] = logkv_detail::hexEncodeLookupLower[byte >> 4];
-        result.data_[i * 2 + 1] =
-          logkv_detail::hexEncodeLookupLower[byte & 0x0F];
-      }
-    }
+    Bytes result(size * 2);
+    logkv::encodeHex(result.data(), result.size(), data, size, upper);
     return result;
   }
 
@@ -138,7 +149,7 @@ public:
   }
 
   static Bytes encodeHex(const Bytes& bytes, bool upper = false) {
-    return encodeHex(bytes.data_, bytes.size_, upper);
+    return encodeHex(bytes.data(), bytes.size(), upper);
   }
 
   Bytes() : data_(nullptr), size_(0) {}
